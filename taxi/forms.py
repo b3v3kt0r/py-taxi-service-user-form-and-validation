e062ff61439503c12b3d237da4692a9b18ef9ff6
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-16 18:32:33 +0300

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -9,7 +9,9 @@ from taxi.models import Driver, Car
 class DriverCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Driver
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "license_number",)
+        fields = UserCreationForm.Meta.fields + ("first_name",
+                                                 "last_name",
+                                                 "license_number",)
 
     def clean_license_number(self):
         return validate_license_number(self.cleaned_data["license_number"])
@@ -26,7 +28,8 @@ class DriverLicenseUpdateForm(UserCreationForm):
 
 def validate_license_number(license_number: str):
     if len(license_number) != 8:
-        raise ValidationError("License number must consist only of 8 characters!")
+        raise ValidationError("License number must "
+                              "consist only of 8 characters!")
     elif license_number[:3] != license_number[:3].upper():
         raise ValidationError("First 3 characters are uppercase letters!")
     elif not license_number[3:].isdigit():
--- taxi/urls.py
+++ taxi/urls.py
@@ -47,14 +47,23 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
-    path("cars/<int:pk>/assign_driver/", car_assign_driver, name="car-assign-driver"),
-    path("cars/<int:pk>/remove_driver/", car_remove_driver, name="car-remove-driver"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
-    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"),
-    path("drivers/<int:pk>/update/", DriverUpdateLicenseView.as_view(), name="driver-update"),
+    path("cars/<int:pk>/assign_driver/",
+         car_assign_driver,
+         name="car-assign-driver"),
+    path("cars/<int:pk>/remove_driver/",
+         car_remove_driver,
+         name="car-remove-driver"),
+    path("drivers/", DriverListView.as_view(),
+         name="driver-list"),
+    path("drivers/create/", DriverCreateView.as_view(),
+         name="driver-create"),
+    path("drivers/<int:pk>/delete/", DriverDeleteView.as_view(),
+         name="driver-delete"),
+    path("drivers/<int:pk>/update/", DriverUpdateLicenseView.as_view(),
+         name="driver-update"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "drivers/<int:pk>/", DriverDetailView.as_view(),
+        name="driver-detail"
     ),
 ]
 
